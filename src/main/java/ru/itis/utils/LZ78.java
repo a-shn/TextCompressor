4a70714179825febe741e3c2936d7b5febffc798
xiu-xiu <sh.il.almaz@gmail.com>
2020-11-26 14:17:11 +0300

    lz78 decoder

--- src/main/java/ru/itis/utils/LZ78.java
+++ src/main/java/ru/itis/utils/LZ78.java
@@ -144,4 +144,22 @@ public class LZ78 {
         }
         return p;
     }
+
+    public List<Integer> decode(LZ78Output lz78Output) {
+        List<Integer> decoded = new ArrayList<>();
+        List<List<Integer>> dict = new ArrayList<>();
+        List<Integer> word = new ArrayList<>();
+        Map<Integer, Integer> alphabetMap = lz78Output.getAlphabetMap();
+        for (LZ78Node node : lz78Output.getNodes()) {
+            if (node.getPos() == 0) {
+                word = new ArrayList<>(Collections.singletonList(alphabetMap.get(node.getNext())));
+            } else {
+                word = new ArrayList<>(dict.get(node.getPos()));
+                word.add(alphabetMap.get(node.getNext()));
+            }
+            dict.add(word);
+            decoded.addAll(word);
+        }
+        return decoded;
+    }
 }
